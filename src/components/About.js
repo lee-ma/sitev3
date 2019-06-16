93722ba83bf60cc7b8923b86d7d111e88769d9c3
lee-ma <leema9321@gmail.com>
2019-06-15 21:17:50 -0700

    fix typo in linkedin link

--- src/components/About.js
+++ src/components/About.js
@@ -9,12 +9,12 @@ const About = () => (
     <p className="text">
       I'm a Computer Engineering student at the University of Waterloo.
       At school, I am an executive member of the <a href="https://entsoc.ca">Entrepreneurship Society</a>,&nbsp;
-      and I was the sole developer of the 2019 <a href="http://enghack.com">EngHack website</a>, a hackathon that received 400+ applications.
+      and I was the sole developer of the 2019 <a href="http://enghack.com">EngHack website</a>, a MLH hackathon that received 560+ applications.
       Outside of school I love any outdoor activity in the mountains, learning to cook new recipes, and going to Costco for soft serve.
     </p>
     <p>
       Learn more about my <a rel="noopener noreferrer" href="https://drive.google.com/file/d/15Mkc4YgmGP5u7kNrnHz_Lgep9JInJxL2/view?usp=sharing" target="_blank">Experience</a> or find me on <a href="https://www.github.com/lee-ma">Github</a>,&nbsp;
-      <a href="https://www.linkedin.com/lma321">LinkedIn</a>.
+      <a href="https://www.linkedin.com/in/lma321">LinkedIn</a>.
     </p>
   </div>
 )
