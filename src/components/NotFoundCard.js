e7d586e1cee1dcb83900677a7f386179854643c0
lee-ma <leema9321@gmail.com>
2019-06-16 23:00:19 -0700

    fix 404

--- src/components/NotFoundCard.js
+++ src/components/NotFoundCard.js
@@ -8,7 +8,7 @@ const Card = () => (
     <div style={{width: "100%"}}>
       <NotFoundImage />
     </div>
-    <h2 style={{marginBottom: "10%", marginTop: "10%", color: "#fff"}}>404! Where are you going, JR?</h2>
+    <h2 style={{marginBottom: "10%", marginTop: "10%"}}>404! Where are you going, JR?</h2>
     <a className="back" href="/">&lt; Take me back</a>
   </div>
 )
