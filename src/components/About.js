3af616d8d1a1c815bf9214dac4132ef5e4e677b1
lee-ma <leema9321@gmail.com>
2019-07-03 20:14:36 -0700

    spacing

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,13 @@ const About = () => (
     <h1>Hi, I'm Lee</h1>
     <p className="text">
       I'm a Computer Engineering student at the University of Waterloo, originally from Calgary, Alberta.
-      At school, I am an executive member of the <a href="https://entsoc.ca">Entrepreneurship Society</a>,&nbsp;
-      and I was the sole developer of the 2019 <a href="http://enghack.com">EngHack website</a>, a MLH hackathon that received 560+ applications.
+      At school, I am an executive member of the <a href="https://entsoc.ca">Entrepreneurship Society</a>,
+      &nbsp;and I was the sole developer of the 2019 <a href="http://enghack.com">EngHack website</a>, a MLH hackathon that received 560+ applications.
       Outside of school I love any outdoor activity in the mountains, learning to cook new recipes, and going to Costco for soft serve.
     </p>
     <p>
-      Learn more about my <a rel="noopener noreferrer" href="leemaresume.pdf" target="_blank">experience</a> and find me on &nbsp;
-      <a href="https://www.github.com/lee-ma">Github</a> or <a href="https://www.linkedin.com/in/lma321">LinkedIn</a>.
+      Learn more about my <a rel="noopener noreferrer" href="leemaresume.pdf" target="_blank">experience</a> and find me on
+      &nbsp;<a href="https://www.github.com/lee-ma">Github</a> or <a href="https://www.linkedin.com/in/lma321">LinkedIn</a>.
     </p>
   </div>
 )
