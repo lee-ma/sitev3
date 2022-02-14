5f286f190af502251d71fc06f2fdf8e1049064f1
lee-ma <leema9321@gmail.com>
2022-02-14 13:04:54 -0500

    update

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => (
     <h1>Hi, I'm Lee 👋</h1>
     <p className="text">
       I'm a Computer Engineering student at the University of Waterloo, originally from Calgary, Alberta.
-      I will be a SWE Intern at Facebook (Instagram) this Fall.
+      I will be joining Meta as a Software Engineer in New York City this Fall.
       At UW, I was an executive member of the <a href="https://entsoc.ca">Entrepreneurship Society</a>, 
       a Project Developer with <a href="https://uwblueprint.org"> UW Blueprint</a>,
       and the developer of the 2019 <a href="http://enghack.com">EngHack website</a>, a MLH hackathon that received 600+ applications.
